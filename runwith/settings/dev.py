decd55bb1962c6b894099f7253c683f15dbff093
thesophile <abhinavpb249@gmail.com>
2026-05-20 12:43:41 +0530

    make random secret key if none given

--- runwith/settings/dev.py
+++ runwith/settings/dev.py
@@ -18,11 +18,17 @@ DATABASES = {
 }
 
 from dotenv import load_dotenv
+from django.core.management.utils import get_random_secret_key
 import os
 
 load_dotenv(BASE_DIR / 'runwith_dev.env') 
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY')
+
 if not SECRET_KEY:
-    raise ValueError("The DJANGO_SECRET_KEY environment variable is not set.")
+    try:
+        SECRET_KEY = get_random_secret_key()
+    except:
+        raise ValueError("The DJANGO_SECRET_KEY environment variable is not set. Do you have runwith_dev.env? See the docs")
+    
