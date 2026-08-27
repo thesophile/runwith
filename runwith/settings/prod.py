a13b9bf9aac732f5eb6eb2ebbdb1b8ac21e81ac3
thesophile <abhinavpb249@gmail.com>
2026-08-27 19:02:34 +0530

    set host and port to null in settings for database to work without password, which is secure and simpler for our case

--- runwith/settings/prod.py
+++ runwith/settings/prod.py
@@ -25,8 +25,8 @@ DATABASES = {
         'NAME': os.getenv('DB_NAME'),
         'USER': os.getenv('DB_USER'),
         'PASSWORD': os.getenv('DB_PASSWORD'),
-        'HOST': 'localhost',
-        'PORT': '5432',
+        'HOST': '',
+        'PORT': '',
     }
 }
 
